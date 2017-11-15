9c7e22111e36ac33d32f3828329db59aa23751e0
Alessandro Manno <alessandro@alessandromanno.info>
2017-11-16 00:20:00 +0100

    Improvements on index.php
    Safer inclusion of routes files on index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ $app = new \Slim\App($container);
 
 $routes = scandir(__DIR__ . '/../app/Routes/');
 foreach ($routes as $route) {
-    if (strpos($route, '.php')) {
+    if (is_file(__DIR__ . '/../app/Routes/' . $route) && mb_substr($route, -4, 4) === '.php') {
         require __DIR__ . '/../app/Routes/' . $route;
     }
 }
